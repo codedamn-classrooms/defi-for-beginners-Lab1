006c9f252ce37d65e9e316462a6be2d88b9d1531
harshita <64593306+harshita808@users.noreply.github.com>
2022-08-04 14:16:22 +0530

    update sol

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -1,14 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-//Few lines of code is given for your help, complete the rest. You can do it :)
 contract Wallet {
 
     event transferred(uint256 amount);
     
-    function send(address payable _addr, uint256 amount) payable public{
-  
-         emit transferred(amount);
-        
-    }
+
 }
