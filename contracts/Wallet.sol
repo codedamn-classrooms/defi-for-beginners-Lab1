9181577faca238aa92c1359ba2a820ea36ae4992
harshita808 <harshitasaxenah7@gmail.com>
2022-08-05 19:57:23 +0530

    Truffle-config

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -2,23 +2,23 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Wallet {
+  address payable public owner;
 
-    event transferred(uint256 amount);
-    
-
-<<<<<<< HEAD
+  event transferred(uint256 amount);
+  constructor(){
+    owner=payable(msg.sender);
+  }
 
   function balanceOf() external view returns (uint){
     return address(owner).balance;
   }
   
     function send(address payable _addr, uint256 amount) payable public{
-       require(amount >= 1 ether);
+        require(amount >= 1 ether);
         require(msg.sender==owner);
         _addr.transfer(amount);
          emit transferred(amount);
         
     }
-=======
->>>>>>> 006c9f252ce37d65e9e316462a6be2d88b9d1531
 }
+
