e683d57c4163e3956c70111f416d2be3e0073c9d
harshita <64593306+harshita808@users.noreply.github.com>
2022-08-13 15:16:08 +0530

    update sol

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -2,26 +2,10 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Wallet {
-  address payable public owner;
-
+  
   event transferred(uint256 amount);
-
-  constructor(address payable _owner){
-    owner=_owner;
-  }
+    
+    //Write your code here
   
-  function deposit() public payable{} 
-
-  function balanceOf() external view returns (uint){
-    return address(this).balance;
-  }
-  
-    function send(address payable _addr, uint256 amount) payable public{
-        require(amount >=1 ether);
-       require(msg.sender==owner);
-        _addr.transfer(amount);
-         emit transferred(amount);
-        
-    }
 }
 
